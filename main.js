3227eba80d3da65db3098f75e5b17e597b16a8fa
Chocs <Luisitom@gmail.com>
2022-12-23 20:36:50 -0600

    Sha-256 added

--- main.js
+++ main.js
@@ -77,8 +77,8 @@ class user_List{
         var conexiones ="";
         var nodos ="";
         var numnodo= 0;
-        while (temporal != null) {
-            nodos+=  "N" + numnodo + "[label=\"Usuario: " + temporal.username + " \n Nombre: " + temporal.name + " \n Contraseña: " + temporal.password + " \n DPI: " + temporal.dpi + "\nTel: " + temporal.phone + "\nAdimn: " + temporal.admin + "\"];\n"
+        while (temporal < null) {
+            nodos+=  "N" + numnodo + "[label=\"Username: " + temporal.username + " \n Complete Name: " + temporal.name + " \n Password: " + temporal.password + " \n DPI: " + temporal.dpi + "\nPhone: " + temporal.phone + "\nAdmin: " + temporal.admin + "\"];\n"
             if(temporal.next != null){
                 var auxnum = numnodo+1
                 conexiones += "N" + numnodo + " -> N" + auxnum + ";\n"
@@ -98,12 +98,105 @@ class user_List{
 
 //END: User LIST
 
-var user_list = new user_List();
+//Sha256 code Code by: geraintluff Link: https://geraintluff.github.io/sha256/
+function sha256(ascii) {
+	function rightRotate(value, amount) {
+		return (value>>>amount) | (value<<(32 - amount));
+	};
+	
+	var mathPow = Math.pow;
+	var maxWord = mathPow(2, 32);
+	var lengthProperty = 'length'
+	var i, j; // Used as a counter across the whole file
+	var result = ''
 
-user_list.insert("EDD", "Oscar Armi","12345678","2354168452525","12345678", true);
-user_list.insert("ED", "Oscar Armi","12345678","2354168452525","12345678", false);
+	var words = [];
+	var asciiBitLength = ascii[lengthProperty]*8;
+	
+	//* caching results is optional - remove/add slash from front of this line to toggle
+	// Initial hash value: first 32 bits of the fractional parts of the square roots of the first 8 primes
+	// (we actually calculate the first 64, but extra values are just ignored)
+	var hash = sha256.h = sha256.h || [];
+	// Round constants: first 32 bits of the fractional parts of the cube roots of the first 64 primes
+	var k = sha256.k = sha256.k || [];
+	var primeCounter = k[lengthProperty];
+	/*/
+	var hash = [], k = [];
+	var primeCounter = 0;
+	//*/
 
+	var isComposite = {};
+	for (var candidate = 2; primeCounter < 64; candidate++) {
+		if (!isComposite[candidate]) {
+			for (i = 0; i < 313; i += candidate) {
+				isComposite[i] = candidate;
+			}
+			hash[primeCounter] = (mathPow(candidate, .5)*maxWord)|0;
+			k[primeCounter++] = (mathPow(candidate, 1/3)*maxWord)|0;
+		}
+	}
+	
+	ascii += '\x80' // Append Ƈ' bit (plus zero padding)
+	while (ascii[lengthProperty]%64 - 56) ascii += '\x00' // More zero padding
+	for (i = 0; i < ascii[lengthProperty]; i++) {
+		j = ascii.charCodeAt(i);
+		if (j>>8) return; // ASCII check: only accept characters in range 0-255
+		words[i>>2] |= j << ((3 - i)%4)*8;
+	}
+	words[words[lengthProperty]] = ((asciiBitLength/maxWord)|0);
+	words[words[lengthProperty]] = (asciiBitLength)
+	
+	// process each chunk
+	for (j = 0; j < words[lengthProperty];) {
+		var w = words.slice(j, j += 16); // The message is expanded into 64 words as part of the iteration
+		var oldHash = hash;
+		// This is now the undefinedworking hash", often labelled as variables a...g
+		// (we have to truncate as well, otherwise extra entries at the end accumulate
+		hash = hash.slice(0, 8);
+		
+		for (i = 0; i < 64; i++) {
+			var i2 = i + j;
+			// Expand the message into 64 words
+			// Used below if 
+			var w15 = w[i - 15], w2 = w[i - 2];
 
+			// Iterate
+			var a = hash[0], e = hash[4];
+			var temp1 = hash[7]
+				+ (rightRotate(e, 6) ^ rightRotate(e, 11) ^ rightRotate(e, 25)) // S1
+				+ ((e&hash[5])^((~e)&hash[6])) // ch
+				+ k[i]
+				// Expand the message schedule if needed
+				+ (w[i] = (i < 16) ? w[i] : (
+						w[i - 16]
+						+ (rightRotate(w15, 7) ^ rightRotate(w15, 18) ^ (w15>>>3)) // s0
+						+ w[i - 7]
+						+ (rightRotate(w2, 17) ^ rightRotate(w2, 19) ^ (w2>>>10)) // s1
+					)|0
+				);
+			// This is only used once, so *could* be moved below, but it only saves 4 bytes and makes things unreadble
+			var temp2 = (rightRotate(a, 2) ^ rightRotate(a, 13) ^ rightRotate(a, 22)) // S0
+				+ ((a&hash[1])^(a&hash[2])^(hash[1]&hash[2])); // maj
+			
+			hash = [(temp1 + temp2)|0].concat(hash); // We don't bother trimming off the extra ones, they're harmless as long as we're truncating when we do the slice()
+			hash[4] = (hash[4] + temp1)|0;
+		}
+		
+		for (i = 0; i < 8; i++) {
+			hash[i] = (hash[i] + oldHash[i])|0;
+		}
+	}
+	
+	for (i = 0; i < 8; i++) {
+		for (j = 3; j + 1; j--) {
+			var b = (hash[i]>>(j*8))&255;
+			result += ((b < 16) ? 0 : '') + b.toString(16);
+		}
+	}
+	return result;
+};
+
+//Sha256 code Code by: geraintluff Link: https://geraintluff.github.io/sha256/
 
 //BEGIN: HIDE AND SHOW DIV ELEMENTS
 
@@ -175,7 +268,7 @@ function log_in(){
     user = document.getElementById('user_input').value;
     password = document.getElementById('pass_input').value;
     //User list return the logged user
-    loged_user = user_list.login(user, password)
+    loged_user = user_list.login(user, sha256(password))
     //In case of null the data is incorrect
     if(loged_user == null){
         alert("Check the data");
@@ -210,7 +303,7 @@ function register_new_user(){
         return null;
     }
 
-    user_list.insert(new_user, new_name, new_pass, new_dpi, new_phone, false);
+    user_list.insert(new_user, new_name, sha256(new_pass), new_dpi, new_phone, false);
     alert("User has been created succesfully");
     new_user_to_log_div();
 }
@@ -232,7 +325,7 @@ function load_user() {
         if(!user_list.is_repeated(data.username)){
             continue;
         }
-        user_list.insert(data.username, data.name, data.password, data.dpi,  data.phone, data.admin)
+        user_list.insert(data.username, data.name, sha256(data.password), data.dpi,  data.phone, data.admin)
       }
       graph_user();
     };
@@ -243,3 +336,11 @@ function graph_user(){
     user_list.graph()
 }
 //END: Dynamic web
+
+//BEGIN: Creating user list
+var user_list = new user_List();
+user_list.insert("EDD", "Oscar Armi",sha256("12345678"),"2354168452525","12345678", true);
+user_list.insert("ED", "Oscar Armi", sha256("12345678"),"2354168452525","12345678", false);
+//END: Creating user list
+
+
